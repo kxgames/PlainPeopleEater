75fbed5e4addb8804d81cce5b7a4d6b6dba0793c
Alex Mitchell <yeuj@comcast.net>
2011-07-03 10:31:56 -0700

    first commit

---
+++ network_interface.py
@@ -0,0 +1,26 @@
+
+
+
+class Network:
+    def __init__ (self, world, host, port):
+        pass
+
+    # received stuff
+    def callback (self, type, callback):
+        pass
+
+    # udp
+    def update (self, time):
+        # Automatically sends positions
+        pass
+
+    # tcp
+    def eat_person (self):
+        pass
+    def game_over (self):
+        pass
+    def flip_roles (self):
+        pass
+
+
+# World has to have "me" and "them" objects
