70e43db3668853e1495c153dcba49f04edaf00b3
Kale Kundert <kale@theKunderts.net>
2011-07-24 22:07:07 -0700

    Increase the acceleration.

--- settings/game.py
+++ settings/game.py
@@ -12,7 +12,7 @@ refresh_rate = 100 / 1000
 size = Rectangle.from_size(500, 500)
 map = Map(size=size, players=2, friction=50)
 
-me = Player("Me", health=10, mass=0.85, force=200, size=20, speed=150)
-you = Player("You", health=10, mass=0.85, force=200, size=20, speed=150)
+me = Player("Me", health=10, mass=0.85, force=400, size=20, speed=150)
+you = Player("You", health=10, mass=0.85, force=400, size=20, speed=150)
 
 button = Button(size=5, timeout=10)
